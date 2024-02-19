9c8ffb1e882267f3dcf1809062e66c8c0c9b6256
shwetapund <pundshweta2020@gmail.com>
2024-02-19 00:44:49 -0800

    create a reducer

---
+++ src/redux/reducers/productReducer.js
@@ -0,0 +1,20 @@
+import {ActionTypes} from "./../contants/action-types";
+
+const initialState = { //object 
+    products:[
+        {
+            id:1,
+            title:"Shweta",
+            category:"programmer",
+        }
+    ] //the products will be an empty array
+} //ye intialstate hein product ki
+
+export const productReducer = (state, {type, payload}) =>{
+    switch (type) {
+        case ActionTypes.SET_PRODUCTS:
+            return state;
+        dafault: 
+            return state;
+    }
+}
